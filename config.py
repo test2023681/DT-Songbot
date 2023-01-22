f81bdf97729bffa58cf756b3a3a222969cf2f513
test2023681 <117702231+test2023681@users.noreply.github.com>
2023-01-22 16:51:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # SlTBrecLand <https://t.me/SLTBrecLand>
 
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID", '22140808'))
+API_HASH = os.getenv("API_HASH", '72e39b383c4655d957f67d4ab94cb3aa')
+BOT_TOKEN = os.getenv("BOT_TOKEN", '5856353439:AAFdSE8ewLkLtmVpcuMDcIquEftCUNhP98w')
